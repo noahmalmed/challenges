ce666e685b595b60843989091fcb600054d009e4
Noah Malmed <nmalmed@adlerplanetarium.org>
2019-02-06 15:59:55 -0600

    Add Some custom authenticated views on the navbar

--- patient-portal/client/components/Navbar.jsx
+++ patient-portal/client/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
 import { withStyles } from 'material-ui/styles';
+import { connect } from 'react-redux';
+import { get } from 'lodash';
+
+import { signOut } from '../actions/auth';
 
 const styles = {
   container: {
@@ -14,19 +18,61 @@ const styles = {
     color: 'white',
     textDecoration: 'none',
   },
+  authenticationView: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  displayName: {
+    margin: 15,
+  },
 };
 
-const Navbar = ({ classes }) => (
+const Navbar = ({ classes, isAuthenticated, displayName, signOut, history }) => (
   <AppBar className={classes.container}>
     <Toolbar>
       <Button><Link to="/" className={classes.button}>Home</Link></Button>
+      {
+        isAuthenticated &&
+          <div className={classes.authenticationView}>
+            <p className={classes.displayName}> { displayName } </p>
+            <Button
+              className={classes.button}
+              onClick={() => {
+                signOut();
+                history.push('/login');
+              }}
+            >
+              Sign Out
+            </Button>
+          </div>
+      }
     </Toolbar>
   </AppBar>
 );
 
 Navbar.propTypes = {
   classes: PropTypes.object.isRequired,
+  isAuthenticated: PropTypes.bool,
+  displayName: PropTypes.string,
+  signOut: PropTypes.func,
+  history: PropTypes.object,
 };
 
+const mapStateToProps = (state) => {
+  const firstName = get(state, 'user.data.firstName', '');
+  const lastName = get(state, 'user.data.lastName', '');
+  return {
+    isAuthenticated: state.user.isAuthenticated,
+    displayName: `${firstName} ${lastName}`,
+  };
+};
 
-export default withStyles(styles)(Navbar);
+const mapDispatchToProps = (dispatch) => ({
+  signOut: () => {
+    dispatch(signOut);
+  },
+});
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Navbar)));
