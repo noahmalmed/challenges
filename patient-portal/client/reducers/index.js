ab7a879db59d1f921d99a29d47f206174d4f16c5
Noah Malmed <nmalmed@adlerplanetarium.org>
2019-02-06 15:52:17 -0600

    Combine all of the new reducers

--- patient-portal/client/reducers/index.js
+++ patient-portal/client/reducers/index.js
@@ -1,7 +1,17 @@
 import { combineReducers } from 'redux';
 
 import user from './auth';
+import patient from './patient';
+import patients from './patients';
+import doctors from './doctor';
+import files from './file';
 
-const rootReducer = combineReducers({ user });
+const rootReducer = combineReducers({
+  user,
+  patient,
+  patients,
+  doctors,
+  files,
+});
 
 export default rootReducer;
